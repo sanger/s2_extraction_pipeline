a0cc589a74cb1e00d454673bebe7211581e7eb24
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2013-11-19 16:48:49 +0000

    Adds focus/blur/skipped element clases to elements in linear process

--- app/scripts/app-components/labware/plate_like.js
+++ app/scripts/app-components/labware/plate_like.js
@@ -89,6 +89,6 @@ define([
   }
 
   function cssForType(type) {
-    return _.isUndefined(type) ? "empty" : type.replace(/[^\w-]+/g, "_").toLowerCase();
+    return (_.isString(type))? type.replace(/[^\w-]+/g, "_").toLowerCase() : "empty";
   }
 });
