7ab6ea198a34135621a373e9e669f7221cd980d8
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-03-11 11:16:14 +0000

    Adds live() style to attach event and keep it active with dom modifications

--- app/scripts/app-components/admin/viewLabware.js
+++ app/scripts/app-components/admin/viewLabware.js
@@ -2,14 +2,11 @@ define([], function () {
   "use strict";
   return function(partial) {
     var html = $('');
-
     var viewer_url = '/s2_admin';
-    $("a[href=#viewLabware]").attr('href', viewer_url);
-    $(document).ready(function() {
-      $("a[href=#viewLabware]").click(function() {
-        window.location.href = viewer_url;
-      });
+    $(document).on('click', "a[href=#viewLabware]", function() {
+      window.location.href = viewer_url;
     });
+    $("a[href=#viewLabware]").attr('href', viewer_url);
     return {
       view: html,
       events: {}
