c0f571d71d8646fca09cdbafa7496999949505d5
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-03-11 15:03:42 +0000

    Added validation to create kit and reset rack list

--- app/scripts/app-components/admin/createKit.js
+++ app/scripts/app-components/admin/createKit.js
@@ -1,6 +1,7 @@
 define([
-  "text!app-components/admin/_createKit.html"
-], function (partialKit) {
+  "text!app-components/admin/_createKit.html",
+  "lib/pubsub"
+], function (partialKit, PubSub) {
   "use strict";
 
   var template= _.template(partialKit);
@@ -27,7 +28,12 @@ define([
           aliquot = getAliquotType(process),
           expires = $(".expires input", html).val(),
           amount = $(".amount input", html).val();
+      if (!barcode.match(/\d*/)) {
+        PubSub.publish("error.status.s2", this, {message: "Incorrect barcode (must be a number without any other extra character, but was <"+barcode+">" });
+        return;
+      }
       app.createKit(barcode, process, aliquot, expires, amount);
+      PubSub.publish("message.status.s2", this, {message: 'Creating kit with barcode <'+barcode+'> for '+aliquot});
     });
     return {
       view: html,
--- app/scripts/app-components/admin/resetRack.js
+++ app/scripts/app-components/admin/resetRack.js
@@ -1,6 +1,7 @@
 define([
-  "text!app-components/admin/_resetRack.html"
-], function (resetRack) {
+  "text!app-components/admin/_resetRack.html",
+  "lib/pubsub"
+], function (resetRack, PubSub) {
   "use strict";
 
   var template= _.template(resetRack);
@@ -8,10 +9,16 @@ define([
   return function(context) {
     var html = $(template(context));
     $("button", html).on("click", function() {
+      if (!$(".barcodes", html).val().match(/^(\d*\n)*\d*$/)) {
+        PubSub.publish("error.status.s2", this, {message: 'Incorrect input (needs to be a list of numbers delimited by newlines)'});
+        return;
+      }
+
       var barcodes = $(".barcodes", html).val().split(/\n/);
       _.each(barcodes, function(barcode)  {
         app.resetRackRoles(barcode);
       });
+      PubSub.publish("message.status.s2", this, {message: 'Input correct. Processing list of barcodes (please wait...)'});
     });
     return {
       view: html,
