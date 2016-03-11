a3a9846324adaa82a92c4e981ad4aafae0494eac
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-03-11 11:08:22 +0000

    Set url for labware viewer.

--- app/scripts/app-components/admin/viewLabware.js
+++ app/scripts/app-components/admin/viewLabware.js
@@ -3,9 +3,11 @@ define([], function () {
   return function(partial) {
     var html = $('');
 
+    var viewer_url = '/s2_admin';
+    $("a[href=#viewLabware]").attr('href', viewer_url);
     $(document).ready(function() {
       $("a[href=#viewLabware]").click(function() {
-        window.location.href = '/s2_admin';
+        window.location.href = viewer_url;
       });
     });
     return {
